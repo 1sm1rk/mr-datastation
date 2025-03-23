bae9b4013440237210067a4657a8ace8c7a24953
Dirk Mueller <smirk.mueller@gmail.com>
2025-03-23 17:28:33 +0100

    inital import

---
+++ src/main/java/de/homelabs/mr/datastation/MrDatastationApplication.java
@@ -0,0 +1,13 @@
+package de.homelabs.mr.datastation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrDatastationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrDatastationApplication.class, args);
+	}
+
+}
